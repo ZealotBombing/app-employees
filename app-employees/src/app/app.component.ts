88940c6614fcdb967a66647e43b4ae5cfe3aef30
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-05-01 00:19:32 -0400

    list employees

--- app-employees/src/app/app.component.ts
+++ app-employees/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './empleado.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app-employees';
+
+  employees: Employee[]=[
+
+    new Employee("Fernanda", "Piña", "Resident", 1000),
+    new Employee("フェルナンダ", "パイナップル", "Resident", 1500),
+    new Employee("페르난다", "파인애플", "Resident", 2000),
+    new Employee("费尔南达", "菠萝", "Resident", 2500),
+    new Employee("फ़र्नेंडा", "अनानास", "Resident", 3000),
+
+
+  ];
 }
---
+++ app-employees/src/app/empleado.model.ts
@@ -0,0 +1,17 @@
+export class Employee{
+
+
+    constructor(name:string, lastName:string, position: string, salary: number){
+
+        this.name=name;
+        this.lastName=lastName;
+        this.position=position;
+        this.salary=salary;
+    }
+
+    name: string ="";
+    lastName: string = "";
+    position: string = "";
+    salary: number = 0;
+    
+}
